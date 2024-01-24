2f86903d3c9ff70ad02b59da931b8a1f1c471d67
Deepak kumar <deepak.kumar.82521@gmail.com>
2024-01-24 07:20:22 +0530

    upload project 5 th time

--- src/component/Videocard.jsx
+++ src/component/Videocard.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 const Videocard = ({data}) => {
     console.log(data);
     const {snippet,statistics}=data || {};
-    const {channelTitle,title,thumbnails}=snippet || {};
+    const {channelTitle,title,thumbnails}=snippet  || {};
   return (
     <div>
         <h2>hello video card</h2>
-        <img src={thumbnails.medium.url} alt={title} />
+        <img src={thumbnails?.medium?.url} alt={title} />
         <ul>
             <li>{channelTitle}</li>
             <li>{title}</li>
-            <li>{statistics.viewCount}</li>
-        </ul>
+            <li>{statistics?.viewCount}</li>
+        </ul> 
     </div>
   )
 }
